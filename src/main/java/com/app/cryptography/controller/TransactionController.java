7b633d0260a8f08f1cad1333b211cfe526d40932
jordan <jordandimst@gmail.com>
2025-03-10 21:59:14 -0300

    create TransactionController

---
+++ src/main/java/com/app/cryptography/controller/TransactionController.java
@@ -0,0 +1,22 @@
+package com.app.cryptography.controller;
+
+import com.app.cryptography.model.Transaction;
+import com.app.cryptography.model.dto.TransactionDto;
+import com.app.cryptography.service.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @PostMapping(value = "/transaction")
+    public ResponseEntity<Transaction> createTransaction(@RequestBody @Valid TransactionDto transactionDto){
+        return ResponseEntity.ok().body(transactionService.saveTransaction(transactionDto.toEntity()));
+    }
+}
