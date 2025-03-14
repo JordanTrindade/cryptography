357f66e31558406a010418c8035f414c759473e1
jordan <jordandimst@gmail.com>
2025-03-13 21:57:38 -0300

    add: encript e decript

--- src/main/java/com/app/cryptography/service/CryptographyService.java
+++ src/main/java/com/app/cryptography/service/CryptographyService.java
@@ -1,16 +1,21 @@
 package com.app.cryptography.service;
 
-import org.jasypt.util.password.StrongPasswordEncryptor;
-import org.jasypt.util.text.BasicTextEncryptor;
 import org.jasypt.util.text.StrongTextEncryptor;
-import org.springframework.stereotype.Service;
 
-public class cryptographyService {
+public class CryptographyService {
     private final static StrongTextEncryptor textEncriptor;
 
     static {
          textEncriptor = new StrongTextEncryptor();
          textEncriptor.setPassword(System.getenv("SECRET-KEY"));
+    }
+
+    public static String encript(String rawText) {
+        return textEncriptor.encrypt(rawText);
+    }
+
+    public static String decript(String encripString) {
+        return textEncriptor.decrypt(encripString);
     }
 
 }
